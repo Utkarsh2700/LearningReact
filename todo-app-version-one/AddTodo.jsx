a422e2fea1de0702a4731c626c1dba6d7cf437cc
Utkarsh2700 <utkarshsharma2718@gmail.com>
2024-03-01 21:44:19 +0530

    basic hardcoded todo add will add further function
    It will updated with time to make it better

---
+++ todo-app-version-one/AddTodo.jsx
@@ -0,0 +1,18 @@
+function AddTodo() {
+  return (
+    <div class="row my-row">
+      <div class="col-6">
+        <input type="text" placeholder="Enter to do here" />
+      </div>
+      <div class="col-4">
+        <input type="date" name="" id="" />
+      </div>
+      <div class="col-2">
+        <button type="button" class="btn btn-success my-button">
+          Add
+        </button>
+      </div>
+    </div>
+  );
+}
+export default AddTodo;
